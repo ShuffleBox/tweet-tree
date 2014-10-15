a5af4f42e2d30ad0d8b750d40cf31a9daf0b68f2
Garret Rumohr <dev@rumohr.me>
2014-10-14 23:49:13 -0500

    it streams really ugly, but it's a start.

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ It is completely conceptual at this point in time, but will be commiting code an
 - Arduino Uno
 - Ethernet Shield
 - Neo Pixel Strip
+
+### Software
+#### TweetStream
+requires
+ 
+- RPi.GPIO
+- [Adafruit RGB LCD Library](https://github.com/adafruit/Adafruit_Python_CharLCD) - Don't forget the setup for i2c and [other configuration requirements](https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c)
--- tweetstream/stream.py
+++ tweetstream/stream.py
@@ -1,18 +1,36 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+
+import Adafruit_CharLCD as LCD
+
 from settings import *
+import ipdb
+
+lcd = LCD.Adafruit_CharLCDPlate()
 
 class listener(StreamListener):
-    
+
     def on_status(self, status):
-        print status.id
-        
+        print status
+        ipdb.set_trace()
+        lcd.message(unicode(status.user.name) + '\n' + unicode(status.text))
+
     def on_error(self, status):
         print status
-        
-auth = OAuthHandler(CKEY, CSECRET)
-auth.set_access_token(ATOKEN, ASECRET)
-twitterStream = Stream(auth, listener())
-#twitterStream.filter(track=["UNT library", "meangreen", "clubwillis"])
-twitterStream.filter(track=["#apple"])
+
+
+def main():
+
+    # Setup Twitter Authentication
+    auth = OAuthHandler(CKEY, CSECRET)
+    auth.set_access_token(ATOKEN, ASECRET)
+
+    # Initialize the stream
+    twitterStream = Stream(auth, listener())
+    #twitterStream.filter(track=["UNT library", "meangreen", "clubwillis"])
+    twitterStream.filter(track=["#apple"])
+
+
+if __name__ == "__main__":
+    main()
